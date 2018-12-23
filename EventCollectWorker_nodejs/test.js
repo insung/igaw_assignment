d0c880e5b3e00ecf2d2e85f6b88ce3d268d8a681
insung <imissyoubrad@gmail.com>
2018-12-23 21:48:35 +0900

    implements sqs (json format patch)

--- EventCollectWorker_nodejs/test.js
+++ EventCollectWorker_nodejs/test.js
@@ -10,26 +10,23 @@ var config = {
 };
 
 exports.handler = async (event) => {
-    var body = JSON.parse(event.Records[0].body);
-    
-    if (body.event_id == null)
-        return 'data not defined';
-
+    //console.log(event);
+    //if (event.event_id == null)
+    //    return 'data not defined';
+        
     var client = new pg.Client(config);
     var rand = Math.floor(Math.random() * (1000 - 1)) + 1;
     var now=dateFormat(new Date(), "yyyy-mm-dd hh:MM:ss");
+    var msg = JSON.stringify(event.Records[0].body, null, 2);
+    var body = JSON.parse(event.Records[0].body);
+    
     await client.connect();
-
-    var queryString = `
-INSERT INTO event_collects (event_id, user_id, event_name, order_id, event_datetime) 
-VALUES ('${body.event_id}_${rand}', '${body.user_id}_${rand}', '${body.event_name}', '${body.parameters.order_id}_${rand}', '${now}');
-
-INSERT INTO parameters (order_id, currency, price) 
-VALUES ('${body.parameters.order_id}_${rand}', '${body.parameters.currency}', ${body.parameters.price});`;
+    
+    var queryString = `INSERT INTO events (msg) VALUES ('${msg}');`;
     
     // db insert
     await client.query(queryString);
     await client.end();
-    
+
     return `Successfully ${rand} processed`;
 };
