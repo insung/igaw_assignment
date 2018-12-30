524785ca4b4d1f65daa07520fdbe1ceaeea68ad0
insung <imissyoubrad@gmail.com>
2018-12-30 21:44:50 +0900

    Get API - orders by userid

--- EventSearchAPI/Controllers/SearchController.cs
+++ EventSearchAPI/Controllers/SearchController.cs
@@ -46,5 +46,28 @@ namespace EventSearchAPI.Controllers
             
             return Ok(result.ToList());
         }
+
+        [HttpGet("orders/{user_id}")]
+        public ActionResult<List<DOEvent>> GetOrdersByUserId(string user_id)
+        {
+            var result = from events in _context.event_collects
+                         join param in _context.parameters on events.order_id equals param.order_id
+                         where events.user_id == user_id
+                         select new
+                         {
+                             event_id = events.event_id,
+                             user_id = events.user_id,
+                             event_name = events.event_name,
+                             parameters = new
+                             {
+                                 order_id = param.order_id,
+                                 currency = param.currency,
+                                 price = param.price
+                             },
+                             event_datetime = events.event_datetime.ToString("yyyy-MM-dd HH:mm:ss")
+                         };
+
+            return Ok(result.ToList());
+        }
     }
 }
