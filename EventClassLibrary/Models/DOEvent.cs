39d142fc0166f2f11178bc9bc737146987f98e21
insung <imissyoubrad@gmail.com>
2018-12-30 21:10:49 +0900

    modified Dataobject class

--- EventClassLibrary/Models/DOEvent.cs
+++ EventClassLibrary/Models/DOEvent.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace EventClassLibrary.Models
 {
     public class DOEvent
     {
+        [Key]
         public string event_id { get; set; }
         public string user_id { get; set; }
         public string event_name { get; set; }
+        public string order_id { get; set; }
+        public DateTime event_datetime { get; set; }
         public DOParameter parameters { get; set; }
     }
 }
--- EventClassLibrary/Models/DOParameter.cs
+++ EventClassLibrary/Models/DOParameter.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace EventClassLibrary.Models
 {
     public class DOParameter
     {
+        [Key]
         public string order_id { get; set; }
         public string currency { get; set; }
         public long price { get; set; }
