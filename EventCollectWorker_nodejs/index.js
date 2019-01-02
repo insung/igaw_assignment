02c161763ee4117c918b1ad93290f404b3ecfe5d
insung <imissyoubrad@gmail.com>
2019-01-02 18:19:50 +0900

    modified event collect worker(lambda)

--- EventCollectWorker_nodejs/index.js
+++ EventCollectWorker_nodejs/index.js
@@ -16,20 +16,19 @@ exports.handler = async (event) => {
         return 'data not defined';
 
     var client = new pg.Client(config);
-    var rand = Math.floor(Math.random() * (1000 - 1)) + 1;
     var now=dateFormat(new Date(), "yyyy-mm-dd hh:MM:ss");
     await client.connect();
 
     var queryString = `
 INSERT INTO event_collects (event_id, user_id, event_name, order_id, event_datetime) 
-VALUES ('${body.event_id}_${rand}', '${body.user_id}_${rand}', '${body.event_name}', '${body.parameters.order_id}_${rand}', '${now}');
+VALUES ('${body.event_id}', '${body.user_id}', '${body.event_name}', '${body.parameters.order_id}', '${now}');
 
 INSERT INTO parameters (order_id, currency, price) 
-VALUES ('${body.parameters.order_id}_${rand}', '${body.parameters.currency}', ${body.parameters.price});`;
+VALUES ('${body.parameters.order_id}', '${body.parameters.currency}', ${body.parameters.price});`;
     
     // db insert
     await client.query(queryString);
     await client.end();
     
-    return `Successfully ${rand} processed`;
+    return `Successfully ${body.user_id} processed`;
 };
